a76bcbd73912daa13f6987e80493da25dd0c3b69
Сабилов Фархад <sabilov_farhad@mail.ru>
2021-08-28 18:05:57 +0300

    entrypoint.sh added waiting for postgres start

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# wait for postgres
+function postgres_ready() {
+python << END
+import sys
+import psycopg2
+
+try:
+    conn = psycopg2.connect(dbname='proddb', user='admin', password='adminpass', host='postgresdb')
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+until postgres_ready; do
+    >&2 echo "Postgres is unavailable - sleeping"
+    sleep 1
+done
+
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py loaddata superuser_fixture.json
